f06e57b3d7cbf194fed118f176165ad46e160f47
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-03-02 18:35:07 -0300

    Teste inserção no banco

---
+++ src/main/java/com/example/foodapi/jpa/CadastroCozinha.java
@@ -0,0 +1,22 @@
+package com.example.foodapi.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+
+import com.example.foodapi.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	public List<Cozinha> listar() {
+		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+	}
+
+}
---
+++ src/test/java/com/example/foodapi/FoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
