563ef19b99dcc258c94e941e8bb4d1cce529452f
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-03-24 23:10:53 -0300

    Customizando resposta Json e Xml

--- src/main/java/com/example/foodapi/domain/model/Cozinha.java
+++ src/main/java/com/example/foodapi/domain/model/Cozinha.java
@@ -6,9 +6,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@JsonRootName("cozinha")
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -19,6 +23,8 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+//	@JsonIgnore
+	@JsonProperty("titulo")
 	@Column(nullable = false)
 	private String nome;
 	
