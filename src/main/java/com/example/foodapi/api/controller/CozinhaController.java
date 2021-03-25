980bb1868909c3545e334203300df46c2d384079
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-03-24 23:48:54 -0300

    DELETE CozinhaController

--- src/main/java/com/example/foodapi/api/controller/CozinhaController.java
+++ src/main/java/com/example/foodapi/api/controller/CozinhaController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +63,23 @@ public class CozinhaController {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
+		try {
+			Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+			
+			if (cozinha != null) {
+				cozinhaRepository.remover(cozinha);
+				
+				return ResponseEntity.noContent().build();
+			}
+			
+			return ResponseEntity.notFound().build();
+		} catch (DataIntegrityViolationException e) { //Da erro se essa cozinha estiver relacionada com algum restaurante
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
 	}
 	
 }
