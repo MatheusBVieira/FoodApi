d556d79b8f420066e81ff8e40a167a56a6c2fcb7
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-03-24 23:22:08 -0300

    Definindo status de resposta HTTP

--- src/main/java/com/example/foodapi/api/controller/CozinhaController.java
+++ src/main/java/com/example/foodapi/api/controller/CozinhaController.java
@@ -3,9 +3,11 @@ package com.example.foodapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.foodapi.domain.model.Cozinha;
@@ -23,6 +25,7 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 	
+	@ResponseStatus(code = HttpStatus.CREATED)
 	@GetMapping("/{cozinhaId}")
 	public Cozinha buscar(@PathVariable Long cozinhaId) {
 		return cozinhaRepository.buscar(cozinhaId);
