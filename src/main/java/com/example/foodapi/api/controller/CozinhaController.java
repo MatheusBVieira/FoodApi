babbef8e283648186858dc44d3caba12939ca142
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-03-24 23:38:12 -0300

    Post CozinhaController

--- src/main/java/com/example/foodapi/api/controller/CozinhaController.java
+++ src/main/java/com/example/foodapi/api/controller/CozinhaController.java
@@ -3,10 +3,14 @@ package com.example.foodapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.foodapi.domain.model.Cozinha;
@@ -35,4 +39,9 @@ public class CozinhaController {
 		return ResponseEntity.notFound().build();
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
 }
