d18ace674a12fc065ee7f890a9233a7936f7790a
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-03-24 23:30:25 -0300

    Resposta HTTP com ResponseEntity

--- src/main/java/com/example/foodapi/api/controller/CozinhaController.java
+++ src/main/java/com/example/foodapi/api/controller/CozinhaController.java
@@ -3,11 +3,10 @@ package com.example.foodapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.foodapi.domain.model.Cozinha;
@@ -25,10 +24,12 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 	
-	@ResponseStatus(code = HttpStatus.CREATED)
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+			
+//		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+		return ResponseEntity.ok(cozinha);
 	}
 	
 }
