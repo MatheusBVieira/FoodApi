74d4c3935f152fa1926c32a24aeacece15a40edb
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-03-24 23:32:57 -0300

    Corrigindo status HTTP para cozinha inexistente

--- src/main/java/com/example/foodapi/api/controller/CozinhaController.java
+++ src/main/java/com/example/foodapi/api/controller/CozinhaController.java
@@ -28,8 +28,11 @@ public class CozinhaController {
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 			
-//		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-		return ResponseEntity.ok(cozinha);
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 }
