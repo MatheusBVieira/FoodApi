f6488da66525b69337a1104ca84d850d8a28f3e0
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-04-02 00:25:30 -0300

    Utilizando @Query no RestauranteRepository

--- src/main/java/com/example/foodapi/api/controller/TesteController.java
+++ src/main/java/com/example/foodapi/api/controller/TesteController.java
@@ -48,7 +48,7 @@ public class TesteController {
 	@GetMapping("/restaurantes/por-nome")
 	public List<Restaurante> restaurantesPorTaxaFrete(
 			String nome, Long cozinhaId) {
-		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+		return restauranteRepository.consultarPorNome(nome, cozinhaId);
 	}
 	
 	@GetMapping("/restaurantes/primeiro-por-nome")
--- src/main/java/com/example/foodapi/domain/repository/RestauranteRepository.java
+++ src/main/java/com/example/foodapi/domain/repository/RestauranteRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.foodapi.domain.model.Restaurante;
@@ -13,12 +15,16 @@ import com.example.foodapi.domain.model.Restaurante;
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
-
-	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
-
-	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome); // Primeiro
-
-	List<Restaurante> findTop2ByNomeContaining(String nome); // Os 2 Primeiros
-
+	
+	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+	
+//	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+	
+	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+	
+	List<Restaurante> findTop2ByNomeContaining(String nome);
+	
 	int countByCozinhaId(Long cozinha);
+	
 }
