4aabffb8853bf6a9df40feb22986a5adfa25d9a0
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-04-02 00:55:50 -0300

    Consulta com filtros dinâmicos com Criteria API

--- src/main/java/com/example/foodapi/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/example/foodapi/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,17 +1,15 @@
 package com.example.foodapi.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.example.foodapi.domain.model.Restaurante;
 import com.example.foodapi.domain.repository.RestauranteRepositoryQueries;
@@ -25,22 +23,28 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	@Override
 	public List<Restaurante> find(String nome, 
 			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		var builder = manager.getCriteriaBuilder();
 		
-		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		Root<Restaurante> root = criteria.from(Restaurante.class);
+		var criteria = builder.createQuery(Restaurante.class);
+		var root = criteria.from(Restaurante.class);
 
-		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+		var predicates = new ArrayList<Predicate>();
 		
-		Predicate taxaInicialPredicate = builder
-				.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		if (StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		}
 		
-		Predicate taxaFinalPredicate = builder
-				.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		if (taxaFreteInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+		}
 		
-		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
+		if (taxaFreteFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		}
 		
-		TypedQuery<Restaurante> query = manager.createQuery(criteria);
+		criteria.where(predicates.toArray(new Predicate[0]));
+		
+		var query = manager.createQuery(criteria);
 		return query.getResultList();
 	}
 	
