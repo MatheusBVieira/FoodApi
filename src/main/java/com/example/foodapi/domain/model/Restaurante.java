fa87d1cbc424b55ba0f223382cd711bf7b5f0cef
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-04-06 18:51:37 -0300

    Alterando estratégia para Eager Loading

--- src/main/java/com/example/foodapi/domain/model/Restaurante.java
+++ src/main/java/com/example/foodapi/domain/model/Restaurante.java
@@ -63,11 +63,11 @@ public class Restaurante {
 	private LocalDateTime dataAtualizacao;
 	
 	@JsonIgnore
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "restaurante_forma_pagamento",
 			joinColumns = @JoinColumn(name = "restaurante_id"),
 			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
-	private List<FormaPagamento> formasPagamento = new ArrayList<>();
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();	
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "restaurante")
