043f94d9671b9c0620641d7806e021ca352bb918
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-04-06 17:08:21 -0300

    Mapeando @Embedded e @Embeddable

--- src/main/java/com/example/foodapi/api/controller/RestauranteController.java
+++ src/main/java/com/example/foodapi/api/controller/RestauranteController.java
@@ -72,7 +72,7 @@ public class RestauranteController {
 					.findById(restauranteId).orElse(null);
 			
 			if (restauranteAtual != null) {
-				BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento");
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco");
 				
 				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
 				return ResponseEntity.ok(restauranteAtual);
--- src/main/java/com/example/foodapi/domain/model/Restaurante.java
+++ src/main/java/com/example/foodapi/domain/model/Restaurante.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -38,6 +39,10 @@ public class Restaurante {
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+	
+	@JsonIgnore
+	@Embedded
+	private Endereco endereco;
 	
 	@JsonIgnore
 	@ManyToMany
