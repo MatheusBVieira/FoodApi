0eb5ae9fc3e9a304ff14d9f008fb0b72868463b7
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-04-08 16:44:52 -0300

    Tratando e customizando exceções no controllador

---
+++ src/main/java/com/example/foodapi/api/exceptionhandler/Problema.java
@@ -0,0 +1,15 @@
+package com.example.foodapi.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Problema {
+
+	private LocalDateTime dataHora;
+	private String mensagem;
+
+}
