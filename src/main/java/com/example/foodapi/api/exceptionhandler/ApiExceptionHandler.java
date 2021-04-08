64071973428b44ef0aa9ad9ddf7db056e41c6681
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-04-08 17:32:09 -0300

    Customizando exception handlers de ResponseEntityExceptionHandler

--- src/main/java/com/example/foodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/example/foodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.foodapi.api.exceptionhandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -14,6 +15,23 @@ import com.example.foodapi.domain.exception.NegocioException;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+		String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+		// Na aula ficou passando uma nova instância de HttpHeaders, mas o ideal
+		// é repassar o "headers" que recebemos como argumento do método
+		// return handleExceptionInternal(ex, problem, new HttpHeaders(), status,
+		// request);
+
+		return handleExceptionInternal(ex, problem, headers, status, request);
+	}
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex,
