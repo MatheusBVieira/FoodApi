03f18d1c74ae8eb6c0220447e008f63f54a0d0d5
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-08-27 12:53:15 -0300

    Formata ApiExceptionHandler

--- src/main/java/com/example/foodapi/api/exceptionhandler/Problem.java
+++ src/main/java/com/example/foodapi/api/exceptionhandler/Problem.java
@@ -1,6 +1,6 @@
 package com.example.foodapi.api.exceptionhandler;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -15,7 +15,7 @@ import lombok.Getter;
 public class Problem {
 
 	private Integer status;
-	private LocalDateTime timestamp;
+	private OffsetDateTime timestamp;
 	private String type;
 	private String title;
 	private String detail;
