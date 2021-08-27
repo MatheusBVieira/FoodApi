5841b0ef6825d0fb0e4a291f279c27bcf1036863
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-08-27 12:22:36 -0300

    Ignora propriedade com JsonIgnoreProperties

--- src/main/java/com/example/foodapi/domain/model/Restaurante.java
+++ src/main/java/com/example/foodapi/domain/model/Restaurante.java
@@ -29,6 +29,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import com.example.foodapi.core.validation.Groups;
 import com.example.foodapi.core.validation.ValorZeroIncluiDescricao;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -53,6 +54,7 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
+	@JsonIgnoreProperties(value = "nome", allowGetters = true)
 	@Valid
 	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
 	@NotNull
