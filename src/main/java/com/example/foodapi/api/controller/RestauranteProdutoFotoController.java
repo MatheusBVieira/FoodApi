6c62dcdc5857b0e1959a6af99046349a910937b8
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-09-29 10:22:42 -0300

    Validando tamanho maximo do arquivo

--- src/main/java/com/example/foodapi/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/example/foodapi/api/controller/RestauranteProdutoFotoController.java
@@ -3,6 +3,8 @@ package com.example.foodapi.api.controller;
 import java.nio.file.Path;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,7 +20,7 @@ public class RestauranteProdutoFotoController {
 
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public void atualizarFoto(@PathVariable Long restauranteId,
-			@PathVariable Long produtoId, FotoProdutoRequest fotoProdutoRequest) {
+			@PathVariable Long produtoId, @Valid FotoProdutoRequest fotoProdutoRequest) {
 		
 		var nomeArquivo = UUID.randomUUID().toString() 
 				+ "_" + fotoProdutoRequest.getArquivo().getOriginalFilename();
--- src/main/java/com/example/foodapi/api/model/request/FotoProdutoRequest.java
+++ src/main/java/com/example/foodapi/api/model/request/FotoProdutoRequest.java
@@ -1,6 +1,11 @@
 package com.example.foodapi.api.model.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.web.multipart.MultipartFile;
+
+import com.example.foodapi.core.validation.FileSize;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +14,11 @@ import lombok.Setter;
 @Setter
 public class FotoProdutoRequest {
 
+	@NotNull
+	@FileSize(max = "500KB")
 	private MultipartFile arquivo;
+	
+	@NotBlank
 	private String descricao;
 	
 }
