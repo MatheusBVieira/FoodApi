35ec9785160b0b5b954a8a1751b13ad7e232e492
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-09-29 16:52:29 -0300

    Cria bean de configuração para armazenagem de fotos

--- src/main/java/com/example/foodapi/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/example/foodapi/infrastructure/service/storage/LocalFotoStorageService.java
@@ -4,17 +4,19 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
+import com.example.foodapi.core.StorageProperties;
+import com.example.foodapi.core.storage.StorageProperties;
 import com.example.foodapi.domain.service.FotoStorageService;
 
 @Service
 public class LocalFotoStorageService implements FotoStorageService {
 
-	@Value("${algafood.storage.local.diretorio-fotos}")
-	private Path diretorioFotos;
+	@Autowired
+	private StorageProperties storageProperties;
 	
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
@@ -51,7 +53,7 @@ public class LocalFotoStorageService implements FotoStorageService {
 	}  
 	
 	private Path getArquivoPath(String nomeArquivo) {
-		return diretorioFotos.resolve(Path.of(nomeArquivo));
+		return storageProperties.getLocal().getDiretorioFotos().resolve(Path.of(nomeArquivo));
 	}
 
 }
