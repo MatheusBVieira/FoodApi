9a92c258256a54fb69848c7dd22b45c2fb799210
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-09-29 16:02:52 -0300

    Implementa endpoint para deletar imagem de produto

--- src/main/java/com/example/foodapi/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/example/foodapi/domain/service/CatalogoFotoProdutoService.java
@@ -56,5 +56,14 @@ public class CatalogoFotoProdutoService {
 	            .orElseThrow(() -> new FotoProdutoNaoEncontradaException(restauranteId, produtoId));
 	}
 	
+	@Transactional
+	public void excluir(Long restauranteId, Long produtoId) {
+	    FotoProduto foto = buscarOuFalhar(restauranteId, produtoId);
+	    
+	    produtoRepository.delete(foto);
+	    produtoRepository.flush();
+
+	    fotoStorage.remover(foto.getNomeArquivo());
+	}
 }
 
