24a19f73d173830b4a65239cedf2677f162b76e6
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-10-18 18:00:13 -0300

    Envia email na confirmação de pedido

--- src/main/java/com/example/foodapi/domain/service/FluxoPedidoService.java
+++ src/main/java/com/example/foodapi/domain/service/FluxoPedidoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.foodapi.domain.model.Pedido;
+import com.example.foodapi.domain.service.EnvioEmailService.Mensagem;
 
 @Service
 public class FluxoPedidoService {
@@ -13,10 +14,22 @@ public class FluxoPedidoService {
 	@Autowired
 	private EmissaoPedidoService emissaoPedido;
 	
+	@Autowired
+	private EnvioEmailService envioEmail;
+	
 	@Transactional
 	public void confirmar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
+		
+		var mensagem = Mensagem.builder()
+				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+				.corpo("O pedido de código <strong>" 
+						+ pedido.getCodigo() + "</strong> foi confirmado!")
+				.destinatario(pedido.getCliente().getEmail())
+				.build();
+		
+		envioEmail.enviar(mensagem);
 	}
 	
 	@Transactional
