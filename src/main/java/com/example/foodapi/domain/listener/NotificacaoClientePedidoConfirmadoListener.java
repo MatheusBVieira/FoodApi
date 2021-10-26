d29da710483f030d1bbe87fe01b4e0a35041a26c
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-10-26 14:18:14 -0300

    Altera eventListener para transactional
    Alterações no banco serão feitas antes de enviar email

--- src/main/java/com/example/foodapi/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/example/foodapi/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -1,8 +1,8 @@
 package com.example.foodapi.domain.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.example.foodapi.domain.event.PedidoConfirmadoEvent;
 import com.example.foodapi.domain.model.Pedido;
@@ -15,7 +15,7 @@ public class NotificacaoClientePedidoConfirmadoListener {
 	@Autowired
 	private EnvioEmailService envioEmail;
 	
-	@EventListener
+	@TransactionalEventListener
 	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
 		Pedido pedido = event.getPedido();
 		
