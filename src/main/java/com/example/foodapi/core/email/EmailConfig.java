ece37625c16adfeec127b4558e6fc695641af3e2
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-10-26 13:49:21 -0300

    Implementa envio de email sandbox

--- src/main/java/com/example/foodapi/core/email/EmailConfig.java
+++ src/main/java/com/example/foodapi/core/email/EmailConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.foodapi.domain.service.EnvioEmailService;
 import com.example.foodapi.infrastructure.service.email.FakeEnvioEmailService;
+import com.example.foodapi.infrastructure.service.email.SandboxEnvioEmailService;
 import com.example.foodapi.infrastructure.service.email.SmtpEnvioEmailService;
 
 @Configuration
@@ -21,6 +22,8 @@ public class EmailConfig {
 				return new FakeEnvioEmailService();
 			case SMTP:
 				return new SmtpEnvioEmailService();
+			case SANDBOX:
+			    return new SandboxEnvioEmailService();
 			default:
 				return null;
 		}
--- src/main/java/com/example/foodapi/core/email/EmailProperties.java
+++ src/main/java/com/example/foodapi/core/email/EmailProperties.java
@@ -16,15 +16,22 @@ import lombok.Setter;
 @ConfigurationProperties("algafood.email")
 public class EmailProperties {
 
-	// Atribuimos FAKE como padrão
-	// Isso evita o problema de enviar e-mails de verdade caso você esqueça
-	// de definir a propriedade
 	private Implementacao impl = Implementacao.FAKE;
+	
+	private Sandbox sandbox = new Sandbox();
 	
 	@NotNull
 	private String remetente;
 	
 	public enum Implementacao {
-		SMTP, FAKE
+	    SMTP, FAKE, SANDBOX
+	}
+	
+	@Getter
+	@Setter
+	public class Sandbox {
+	    
+	    private String destinatario;
+	    
 	}
 }
