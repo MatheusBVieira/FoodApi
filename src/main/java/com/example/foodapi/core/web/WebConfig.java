94da04426b572a4c568a629559d337ecf57c1b1c
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-10-27 15:22:14 -0300

    Implementa cache com ETag

--- src/main/java/com/example/foodapi/core/web/WebConfig.java
+++ src/main/java/com/example/foodapi/core/web/WebConfig.java
@@ -1,6 +1,10 @@
 package com.example.foodapi.core.web;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addMapping("/**")
 				.allowedMethods("*");
 //				.allowedOrigins("http://www.algafood.local:8000")
+	}
+	
+	@Bean
+	public Filter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
 	}
 	
 }
