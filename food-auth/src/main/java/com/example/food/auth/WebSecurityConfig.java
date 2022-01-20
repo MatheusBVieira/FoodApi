96016298b70e395755b3ac09b630e7d9fc62674f
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2022-01-19 22:50:17 -0300

    Configura AuthorizationServer com Password Credentials

--- food-auth/src/main/java/com/example/food/auth/WebSecurityConfig.java
+++ food-auth/src/main/java/com/example/food/auth/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.food.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -27,6 +28,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 	
 }
