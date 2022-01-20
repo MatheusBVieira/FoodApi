e10168cfe55f0b68e05b87b6957506fbe25929e7
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2022-01-19 22:18:33 -0300

    Cria projeto do Authorization Server com Spring Security OAuth2

---
+++ food-auth/src/main/java/com/example/food/auth/AuthorizationServerConfig.java
@@ -0,0 +1,11 @@
+package com.example.food.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+}
---
+++ food-auth/src/main/java/com/example/food/auth/FoodAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.food.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAuthApplication.class, args);
+	}
+
+}
