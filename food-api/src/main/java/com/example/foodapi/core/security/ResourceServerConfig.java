f8f77fab2e7e166192dedbd5724e467b211e1f4c
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2022-01-21 15:44:32 -0300

    Libera cors

--- food-api/src/main/java/com/example/foodapi/core/security/ResourceServerConfig.java
+++ food-api/src/main/java/com/example/foodapi/core/security/ResourceServerConfig.java
@@ -15,6 +15,7 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 				.anyRequest().authenticated()
 			.and()
+			.cors().and()
 			.oauth2ResourceServer().opaqueToken();
 	}
 	
