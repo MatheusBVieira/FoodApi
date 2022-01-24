6dc9f125f722ae9572d8b2aea7333e6f2e8eb585
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2022-01-24 14:17:15 -0300

    Adiciona infos customizadas no payload do JWT

--- food-auth/src/main/java/com/example/food/auth/core/AuthUser.java
+++ food-auth/src/main/java/com/example/food/auth/core/AuthUser.java
@@ -12,11 +12,13 @@ public class AuthUser extends User {
 
 	private static final long serialVersionUID = 1L;
 	
+	private Long userId;
 	private String fullName;
 	
 	public AuthUser(Usuario usuario) {
 		super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
 		
+		this.userId = usuario.getId();
 		this.fullName = usuario.getNome();
 	}
 	
--- food-auth/src/main/java/com/example/food/auth/core/AuthorizationServerConfig.java
+++ food-auth/src/main/java/com/example/food/auth/core/AuthorizationServerConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
@@ -83,11 +84,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		var enhancerChain = new TokenEnhancerChain();
+		enhancerChain.setTokenEnhancers(
+				Arrays.asList(new JwtCustomClaimsTokenEnhancer(), jwtAccessTokenConverter()));
+		
 		endpoints
 			.authenticationManager(authenticationManager)
 			.userDetailsService(userDetailsService)
 			.reuseRefreshTokens(false)
 			.accessTokenConverter(jwtAccessTokenConverter())
+			.tokenEnhancer(enhancerChain)
 			.approvalStore(approvalStore(endpoints.getTokenStore()))
 			.tokenGranter(tokenGranter(endpoints));
 	}
