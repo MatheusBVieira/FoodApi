a8587a19235db52aff1b745cc3ee3f9cc1f29ae5
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2022-01-27 12:35:12 -0300

    fix: allowedOrigins cannot contain the special value "*"

--- food-api/src/main/java/com/example/foodapi/core/security/CorsConfig.java
+++ food-api/src/main/java/com/example/foodapi/core/security/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 	public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedOriginPatterns(Collections.singletonList("*"));
 		config.setAllowedMethods(Collections.singletonList("*"));
 		config.setAllowedHeaders(Collections.singletonList("*"));
 		
