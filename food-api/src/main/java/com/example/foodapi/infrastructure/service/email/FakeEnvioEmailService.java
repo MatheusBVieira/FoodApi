f92b91704ac727bcaef8230cbe4a690d64cb1e09
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2022-01-27 16:18:31 -0300

    Dependência de JavaMailSender não satisfeita: melhorando o uso da herança

--- food-api/src/main/java/com/example/foodapi/infrastructure/service/email/FakeEnvioEmailService.java
+++ food-api/src/main/java/com/example/foodapi/infrastructure/service/email/FakeEnvioEmailService.java
@@ -1,16 +1,22 @@
 package com.example.foodapi.infrastructure.service.email;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.foodapi.domain.service.EnvioEmailService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class FakeEnvioEmailService extends SmtpEnvioEmailService {
+public class FakeEnvioEmailService implements EnvioEmailService {
 
-    @Override
-    public void enviar(Mensagem mensagem) {
-        // Foi necessário alterar o modificador de acesso do método processarTemplate
-        // da classe pai para "protected", para poder chamar aqui
-        String corpo = processarTemplate(mensagem);
+	@Autowired
+	private ProcessadorEmailTemplate processadorEmailTemplate;
+	
+	@Override
+	public void enviar(Mensagem mensagem) {
+		String corpo = processadorEmailTemplate.processarTemplate(mensagem);
 
-        log.info("[FAKE E-MAIL] Para: {}\n{}", mensagem.getDestinatarios(), corpo);
-    }
+		log.info("[FAKE E-MAIL] Para: {}\n{}", mensagem.getDestinatarios(), corpo);
+	}
+
 }
--- food-api/src/main/java/com/example/foodapi/infrastructure/service/email/SmtpEnvioEmailService.java
+++ food-api/src/main/java/com/example/foodapi/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -6,13 +6,9 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import com.example.foodapi.core.email.EmailProperties;
 import com.example.foodapi.domain.service.EnvioEmailService;
-
-import freemarker.template.Configuration;
-import freemarker.template.Template;
 
 public class SmtpEnvioEmailService implements EnvioEmailService {
 
@@ -23,42 +19,31 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	private EmailProperties emailProperties;
 	
 	@Autowired
-	private Configuration freemarkerConfig;
+	private ProcessadorEmailTemplate processadorEmailTemplate;
 	
 	@Override
 	public void enviar(Mensagem mensagem) {
-	    try {
-	        MimeMessage mimeMessage = criarMimeMessage(mensagem);
-	        
-	        mailSender.send(mimeMessage);
-	    } catch (Exception e) {
-	        throw new EmailException("Não foi possível enviar e-mail", e);
-	    }
-	}
-
-	protected MimeMessage criarMimeMessage(Mensagem mensagem) throws MessagingException {
-	    String corpo = processarTemplate(mensagem);
-	    
-	    MimeMessage mimeMessage = mailSender.createMimeMessage();
-	    
-	    MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
-	    helper.setFrom(emailProperties.getRemetente());
-	    helper.setTo(mensagem.getDestinatarios().toArray(new String[0]));
-	    helper.setSubject(mensagem.getAssunto());
-	    helper.setText(corpo, true);
-	    
-	    return mimeMessage;
-	}
-	
-	protected String processarTemplate(Mensagem mensagem) {
 		try {
-			Template template = freemarkerConfig.getTemplate(mensagem.getCorpo());
+			MimeMessage mimeMessage = criarMimeMessage(mensagem);
 			
-			return FreeMarkerTemplateUtils.processTemplateIntoString(
-					template, mensagem.getVariaveis());
+			mailSender.send(mimeMessage);
 		} catch (Exception e) {
-			throw new EmailException("Não foi possível montar o template do e-mail", e);
+			throw new EmailException("Não foi possível enviar e-mail", e);
 		}
 	}
-
+	
+	protected MimeMessage criarMimeMessage(Mensagem mensagem) throws MessagingException {
+		String corpo = processadorEmailTemplate.processarTemplate(mensagem);
+		
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+		
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+		helper.setFrom(emailProperties.getRemetente());
+		helper.setTo(mensagem.getDestinatarios().toArray(new String[0]));
+		helper.setSubject(mensagem.getAssunto());
+		helper.setText(corpo, true);
+		
+		return mimeMessage;
+	}
+	
 }
