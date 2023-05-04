405428dbb962a0c6bdcf44a5df8702548d1f0a03
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2023-05-04 01:16:57 -0300

    Descreve as operações de endpoints na documentação

--- food-api/src/main/java/com/example/foodapi/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ food-api/src/main/java/com/example/foodapi/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -5,6 +5,7 @@ import org.springframework.hateoas.CollectionModel;
 import com.example.foodapi.api.v1.model.request.CidadeRequest;
 import com.example.foodapi.api.v1.model.response.CidadeResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -12,14 +13,20 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Cidades")
 public interface CidadeControllerOpenApi {
 
+	@Operation(summary = "Lista as cidades")
 	CollectionModel<CidadeResponse> listar();
 	
+	@Operation(summary = "Busca uma cidade por Id")
 	CidadeResponse buscar(Long cidadeId);
 	
+	@Operation(summary = "Cadastra uma cidade", description = "Cadastro de uma cidade, " +
+			"necessita de um estado e um nome válido")
 	CidadeResponse adicionar(CidadeRequest cidadeRequest);
-	
+
+	@Operation(summary = "Atualizado uma cidade por ID")
 	CidadeResponse atualizar(Long cidadeId,CidadeRequest cidadeRequest);
 	
+	@Operation(summary = "Excluir uma cidade por ID")
 	void remover(Long cidadeId);
 	
 }
