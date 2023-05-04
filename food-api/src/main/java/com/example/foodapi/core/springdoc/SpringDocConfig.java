a213d57a68139048efaf8b1f946cc33db3c9c489
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2023-05-04 00:41:44 -0300

    Implementa configuração do springdoc

---
+++ food-api/src/main/java/com/example/foodapi/core/springdoc/SpringDocConfig.java
@@ -0,0 +1,31 @@
+package com.example.foodapi.core.springdoc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("FoodAPI")
+                        .version("v1")
+                        .description("REST API")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.com")
+                        )
+                ).externalDocs(new ExternalDocumentation()
+                        .description("AlgaWorks")
+                        .url("https://algaworks.com")
+                );
+    }
+
+}
