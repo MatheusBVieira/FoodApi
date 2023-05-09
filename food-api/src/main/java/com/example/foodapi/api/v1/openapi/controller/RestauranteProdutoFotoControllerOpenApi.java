3eedcde02cb715505ca8408f5cb2be3b42b06957
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2023-05-09 00:07:31 -0300

    Descreve media type da resposta nos endpoints

--- food-api/src/main/java/com/example/foodapi/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
+++ food-api/src/main/java/com/example/foodapi/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
@@ -9,6 +9,10 @@ import org.springframework.web.multipart.MultipartFile;
 import com.example.foodapi.api.v1.model.request.FotoProdutoRequest;
 import com.example.foodapi.api.v1.model.response.FotoProdutoResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @SecurityRequirement(name="security_auth")
@@ -19,8 +23,16 @@ public interface RestauranteProdutoFotoControllerOpenApi {
 
 	void excluir(Long restauranteId, Long produtoId);
 
+	@Operation(summary = "Busca a foto do produto de um restaurante", responses = {
+			@ApiResponse(responseCode = "200", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = FotoProdutoResponse.class)),
+					@Content(mediaType = "image/jpeg", schema = @Schema(type = "string", format = "binary")),
+					@Content(mediaType = "image/png", schema = @Schema(type = "string", format = "binary"))
+			})
+	})
 	FotoProdutoResponse buscar(Long restauranteId, Long produtoId);
 
+	@Operation(hidden = true)
 	ResponseEntity<?> servir(Long restauranteId, Long produtoId, String acceptHeader)
 			throws HttpMediaTypeNotAcceptableException;
 }
