c87ea2edae7ae132e28af460589fa380cede90df
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2023-05-08 23:50:40 -0300

    Descreve documentação de endpoints de formas de pagamento

--- food-api/src/main/java/com/example/foodapi/api/v1/model/request/FormaPagamentoRequest.java
+++ food-api/src/main/java/com/example/foodapi/api/v1/model/request/FormaPagamentoRequest.java
@@ -2,6 +2,7 @@ package com.example.foodapi.api.v1.model.request;
 
 import javax.validation.constraints.NotBlank;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import lombok.Setter;
 public class FormaPagamentoRequest {
 	
 	@NotBlank
+	@Schema(example = "Cartão de crédito")
     private String descricao;
 
 }
--- food-api/src/main/java/com/example/foodapi/api/v1/openapi/controller/FormaPagamentoControllerOpenApi.java
+++ food-api/src/main/java/com/example/foodapi/api/v1/openapi/controller/FormaPagamentoControllerOpenApi.java
@@ -7,18 +7,48 @@ import org.springframework.web.context.request.ServletWebRequest;
 import com.example.foodapi.api.v1.model.request.FormaPagamentoRequest;
 import com.example.foodapi.api.v1.model.response.FormaPagamentoResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name="security_auth")
+@Tag(name = "Formas de pagamento")
 public interface FormaPagamentoControllerOpenApi {
 
+	@Operation(summary = "Lista as formas de pagamento")
 	ResponseEntity<CollectionModel<FormaPagamentoResponse>> listar(ServletWebRequest request);
 
-	ResponseEntity<FormaPagamentoResponse> buscar(Long formaPagamentoId, ServletWebRequest request);
+	@Operation(summary = "Busca uma forma de pagamento por ID", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID da forma de pagamento inválido", content = {
+					@Content(schema = @Schema(ref = "Problema")) }),
+			@ApiResponse(responseCode = "404", description = "Forma de pagamento não encontrada", content = {
+					@Content(schema = @Schema(ref = "Problema")) })
+	})
+	ResponseEntity<FormaPagamentoResponse> buscar(@Parameter(description = "ID de uma forma de pagamento", example = "1", required = true) Long formaPagamentoId,
+			@Parameter(hidden = true) ServletWebRequest request);
 
-	FormaPagamentoResponse adicionar(FormaPagamentoRequest formaPagamentoRequest);
+	@Operation(summary = "Cadastra uma forma de pagamento", responses = {
+			@ApiResponse(responseCode = "201", description = "Forma de pagamento cadastrada")})
+	FormaPagamentoResponse adicionar(@RequestBody(description = "Representação de uma nova forma de pagamento", required = true) FormaPagamentoRequest formaPagamentoRequest);
 
-	FormaPagamentoResponse atualizar(Long formaPagamentoId, FormaPagamentoRequest formaPagamentoRequest);
+	@Operation(summary = "Atualiza uma forma de pagamento por ID", responses = {
+			@ApiResponse(responseCode = "200", description = "Forma de pagamento atualizada"),
+			@ApiResponse(responseCode = "404", description = "Forma de pagamento não encontrada", content = {
+					@Content(schema = @Schema(ref = "Problema")) })
+	})
+	FormaPagamentoResponse atualizar(@Parameter(description = "ID de uma forma de pagamento", example = "1", required = true) Long formaPagamentoId,
+			@RequestBody(description = "Representação de uma forma de pagamento com os novos dados", required = true) FormaPagamentoRequest formaPagamentoRequest);
 
-	void remover(Long formaPagamentoId);
+	@Operation(summary = "Exclui uma forma de pagamento por ID", responses = {
+			@ApiResponse(responseCode = "204", description = "Forma de pagamento excluída"),
+			@ApiResponse(responseCode = "404", description = "Forma de pagamento não encontrada", content = {
+					@Content(schema = @Schema(ref = "Problema")) })
+	})
+	void remover(@Parameter(description = "ID de uma forma de pagamento", example = "1", required = true) Long formaPagamentoId);
 }
