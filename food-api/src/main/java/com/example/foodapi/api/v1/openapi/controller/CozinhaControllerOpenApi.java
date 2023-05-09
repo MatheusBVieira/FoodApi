ffddf6366a50247723022c9a32856cab2291bdf9
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2023-05-08 23:42:15 -0300

    Corrige documentação com substituição de Pageable

--- food-api/src/main/java/com/example/foodapi/api/v1/openapi/controller/CozinhaControllerOpenApi.java
+++ food-api/src/main/java/com/example/foodapi/api/v1/openapi/controller/CozinhaControllerOpenApi.java
@@ -5,13 +5,16 @@ import org.springframework.hateoas.PagedModel;
 
 import com.example.foodapi.api.v1.model.request.CozinhaRequest;
 import com.example.foodapi.api.v1.model.response.CozinhaResponse;
+import com.example.foodapi.core.springdoc.PageableParameter;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @SecurityRequirement(name="security_auth")
 public interface CozinhaControllerOpenApi {
 
-	PagedModel<CozinhaResponse> listar(Pageable pageable);
+	@PageableParameter
+	PagedModel<CozinhaResponse> listar(@Parameter(hidden = true) Pageable pageable);
 
 	CozinhaResponse buscar(Long cozinhaId);
 
